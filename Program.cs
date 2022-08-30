9cb2cf7ffaa93c4112111667f2b71313f49df211
JhonM <jhon@example.com>
2022-08-30 18:24:03 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab02
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            int valor1 = 0;
+            int valor2 = 0;
+            int valor3 = 0;
+            int valor4 = 0;
+            int valor5 = 0;
+            int valor6 = 0;
+            int valor7 = 0;
+            int valor8 = 0;
+            Console.WriteLine("Ingrese x1");
+            valor1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese y1");
+            valor2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese x2");
+            valor3 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese y2");
+            valor4 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese x3");
+            valor5 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese y3");
+            valor6 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese x4");
+            valor7 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese y4");
+            valor8 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Valores Ingresados correctamente ");
+
+
+            Rectangulo rectangulo = new Rectangulo
+            {
+                x1 = valor1,
+                y1 = valor2,
+                x2 = valor3,
+                y2 = valor4,
+                x3 = valor5,
+                y3 = valor6,
+                x4 = valor7,
+                y4 = valor8,
+
+
+
+            };
+
+            // VALORES
+
+            Console.WriteLine("El area del rectangulo  es :");
+            Console.WriteLine(rectangulo.area());
+
+
+            Console.WriteLine("El perimetro del rectangulo  es :");
+            Console.WriteLine(rectangulo.perimetro());
+
+
+            Console.Read();
+        }
+    }
+}
---
+++ Rectangulo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab02
+{
+    public class Rectangulo
+    {
+        public int x1 { get; set; }
+        public int y1 { get; set; }
+
+        public int x2 { get; set; }
+        public int y2 { get; set; }
+
+        public int x3 { get; set; }
+        public int y3 { get; set; }
+
+        public int x4 { get; set; }
+        public int y4 { get; set; }
+
+        public double area()
+        {
+            double altura = Math.Sqrt((Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2)));
+            double baseee = Math.Sqrt((Math.Pow(x2 - x3, 2) + Math.Pow(y2 - y3, 2)));
+            return altura*baseee;
+        }
+
+        public double perimetro()
+        {
+            double alt = Math.Sqrt((Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2)));
+            double bs = Math.Sqrt((Math.Pow(x2 - x3, 2) + Math.Pow(y2 - y3, 2)));
+            double sumatotal = 2 * alt + 2 * bs;
+            return sumatotal;
+        }
+
+    }
+}
